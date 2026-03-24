a709bae8f516f3bf7bfd32f43d71310059320c13
alumno <alumno@usal.es>
2026-03-24 12:58:49 +0100

    Segundo día - termalización y medidas. ¿Distancia ok? Falta medir cada 20 pasos

--- Simulation.f
+++ Simulation.f
@@ -1,25 +1,37 @@
       program MAIN
       Implicit NONE
-      real*8 L, ran, deltamax, pi
-      integer Np, Nmc, Nes, Nmeas, i, j, k
+      real*8 L, ran, deltamax, pi, dist, rbin, r1(3), r2(3)
+      integer Np, Nmc, Nes, Nmeas, i, j, k, nbin
       logical sol
       
       ! Usamos .d0 para asegurar que son reales de doble precisión
-      parameter (L=10.d0, Np=100, Nmc=100000, deltamax=L/100.d0
-     &, pi=4.d0*datan(1.d0))
-      real*8 r(Np, 3), rnew(3)
+      parameter (L=14.d0, Np=256, Nmc=1000000, deltamax=L/100.d0
+     &, pi=4.d0*datan(1.d0), rbin=1.d0, 
+     & nbin=int(l/rbin*dsqrt(3.d0)/2.d0))
+      real*8 r(Np, 3), rnew(3), bines(0:nbin)
       print *, Np*4.d0/3*pi/(L**3)
 
-      Nmeas = 0
-      Nes = 0
+      Nmeas = int(Np**(1.d0/3))
+      Nes = Np
 
       ! 1. Inicialización de posiciones
       ! Usamos dble() para evitar el error de la división entera en Fortran (cambiar inicialización)
-      do i=1,Np
-        do j=1,3
-          r(i,j) = L * dble(i) / dble(Np)
+      do i=0,Nmeas
+        do j=0,Nmeas
+          do k= 0,Nmeas
+            r(Nes,1) = 1+(2+(L-Nmeas*2)/(Nmeas))*i
+            r(Nes,2) = 1+(2+(L-Nmeas*2)/(Nmeas))*j
+            r(Nes,3) = 1+(2+(L-Nmeas*2)/(Nmeas))*k
+
+            Nes = Nes - 1
+            if (Nes.eq.0) goto 999
+          end do
         end do
       end do
+999   continue
+
+      Nmeas = 0
+      Nes = 0
       
       ! 2. Bucle principal de Monte Carlo
       do k=1, Nmc
@@ -44,7 +56,7 @@
         Nes = Nes + 1
         
         ! Comprobar solapamientos con las demás partículas
-        call distance(Np, i, r, rnew, L, sol)
+        call solap(Np, i, r, rnew, L, sol)
         
         ! Aceptar o rechazar
         if (sol) then
@@ -54,35 +66,61 @@
           end do
         end if
       end do
-      
+
       ! Imprimir tasa de aceptación final
       print *, "Tasa de aceptacion: ", Nmeas * 1.d0 / Nes
       
+      do i=1,Np
+        do j=1,i
+          do k=1,3
+            r1(k)=r(i,k)
+            r2(k)=r(j,k)
+          end do
+          call distance(r1, r2, L, dist)
+          bines(int(dist/rbin)) = bines(int(dist/rbin)) + 2 ! Pendiente inicializar a 0 bines
+        end do  
+      end do
+
       end program
 
       ! 3. Subrutina para calcular la distancia e imagen mínima
-      SUBROUTINE distance(Np, i, r, rnew, L, sol)
+      SUBROUTINE solap(Np, i, r, rnew, L, sol)
       implicit NONE
       integer i, Np, j, k
-      real*8 r(Np, 3), rnew(3), L, d(3)
+      real*8 r(Np, 3), rnew(3), L, dist, r1(3)
       logical sol
 
       sol = .true.
       do k=1, Np
         if (k .ne. i) then
-          do j=1, 3
-            d(j) = dabs(rnew(j) - r(k,j))
-            if (d(j) .gt. L / 2.d0) then
-              d(j) = L - d(j)
-            end if
+          do j=1,3
+            r1(j)=r(k,j)
           end do
+
+          call distance(r1,rnew, L, dist)
           
           ! Comprobar solapamiento
-          if (dsqrt(d(1)**2 + d(2)**2 + d(3)**2) .lt. 2.d0) then
+          if (dist .lt. 2.d0) then
             sol = .false.
             EXIT
+
           end if
         end if
       end do
+      END SUBROUTINE
+
+      SUBROUTINE distance( r1, r2, L, dist)
+      implicit NONE
+      integer k
+      real*8 r1(3), r2(3), L, d(3), dist
+
+      do k=1, 3
+        d(k) = dabs(r1(k) - r2(k))
+        if (d(k) .gt. L / 2.d0) then
+          d(k) = L - d(k)
+        end if
+      end do
+      dist = dsqrt(d(1)**2 + d(2)**2 + d(3)**2)
+
       END SUBROUTINE
 
