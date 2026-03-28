2fac28a8747dbe1649ecd277759eb48e9063f1ac
pablolopse <pmpls13@gmail.com>
2026-03-28 14:57:14 +0100

    Corrección de bugs introducidos en el último commit:  - Inicialización errónea del cubo  - Inicialización con overlap Mejoras hechas:  - Introducción de la definición correcta de paso    Montecarlo. Ahora se definen pasos MC y se    itera sobre cada partícula antes de hacer un paso.    Además, solo se avanza cuando se produce y acepta    un movimiento, no en cualquier caso.  - Se han añadido comentarios múltiples, explicando lógico.  - Se ha añadido el uso de una variable radio que hacía falta.  - Se ha corregido parte del código al inglés para usar un idioma único.

--- Simulation.f
+++ Simulation.f
@@ -2,88 +2,115 @@
       implicit NONE
       
       ! Parameters and Variables
-      real*8 L, ran, deltamax, pi, dist, rbin, rho, r_in, r_out
-      real*8 r_centro, V_capa, g_r, vol_frac
-      integer Np, Nmc, Nmov, i, j, k, nbin, Nbines, Nmeas, Ntarg, n, m
-      logical sol
+      REAL*8 L, ran, deltamax, pi, dist, rbin, rho, r_in, r_out
+      REAL*8 r_centro, V_capa, g_r, vol_frac, radius
+      INTEGER Np, STEPS, Nmov, Ntarg, n, initCubeSide, Nbins, Nmeas
+      INTEGER i, j, k, m ! Indices for the program. m for particles,
+      ! i, j, k for coordinates
+      LOGICAL nOverlap
       
       ! Parameters setup
-      parameter (L=14.d0, Np=256, Nmc=100000, deltamax=0.2d0)
-      parameter (pi=4*datan(1.d0), rbin=0.1d0)
+      PARAMETER (L=14.d0, Np=256, STEPS=1000, deltamax=0.2d0)
+      PARAMETER (pi=4*datan(1.d0), rbin=0.1d0, radius=1.d0)
       
       ! Arrays
-      real*8 r(Np, 3), rnew(3), bines(0:500)
+      REAL*8 r(Np, 3), rnew(3), bins(0:500) ! Positions of every part.,
+      ! new generated move for one and bins to store g(r)
       
-      ! Calculate Volume Fraction (phi) for info
-      vol_frac = (Np * (4.d0/3.d0) * pi * (1.d0**3)) / (L**3)
-      print *, "Volume Fraction (phi): ", vol_frac
+      ! Calculate Fraction of occupied volume with cubeside L and Np s.
+      vol_frac = (Np * (4.d0/3.d0) * pi * (radius**3)) / (L**3)
+      print *, "Occupied Volume Fraction: ", vol_frac
       
-      ! 1. Initialization: Simple Cubic Lattice
-      ! This ensures particles start without overlaps
-      nbin = nint(Np**(1.d0/3.d0))
-      k = 1
-      do i=0, nbin-1
-        do j=0, nbin-1
-          do m=0, nbin-1
-            if (k .le. Np) then
-              r(k,1) = i * (L/nbin) + 0.5d0
-              r(k,2) = j * (L/nbin) + 0.5d0
-              r(k,3) = m * (L/nbin) + 0.5d0
-              k = k + 1
+
+      ! ============================================
+      ! =           1. Initialization              =
+      ! ============================================
+      ! This ensures particles start without overlaps. We are
+      ! initializing to a simple cubic lattice with equal separation
+      initCubeSide = int(Np**(1.d0/3.d0)) ! Max cube side needed
+      m = 1
+      do i=0, initCubeSide
+        do j=0, initCubeSide
+          do k=0, initCubeSide
+            if (m .le. Np) then
+              r(m,1) = i * (L/initCubeSide) + 0.5d0
+              r(m,2) = j * (L/initCubeSide) + 0.5d0
+              r(m,3) = k * (L/initCubeSide) + 0.5d0
+              m = m + 1
+              ! If we have initialized all particles, exit loops
+              if (m.eq.Np+1) goto 999
             end if
           end do
         end do
       end do
+999   continue
 
-      ! 2. Thermalization / Equilibration
-      Nmov = 0
-      do k=1, Nmc
-        call random_number(ran)
-        i = int(ran * Np) + 1
-        
-        do j=1,3
-          call random_number(ran)
-          rnew(j) = r(i,j) + deltamax * (2.d0 * ran - 1.d0)
-          ! Periodic Boundary Conditions
-          if (rnew(j) .ge. L) rnew(j) = rnew(j) - L
-          if (rnew(j) .lt. 0.d0) rnew(j) = rnew(j) + L
+
+      ! ============================================
+      ! =    2. Thermalization / Equilibration     =
+      ! ============================================
+      Nmov = 0 ! Number of accepted, total performed moves
+      do k=1, STEPS    ! We are going to perform STEPS MC steps
+        do j=1, Np     ! For each MC step, iterate over every particle
+998       call random_number(ran) ! We do a random particle selection
+          m = int(ran * Np) + 1 ! int() produces from 0 to Np-1, thus +1
+          
+          ! We generate a random movement in each direction from 0 to
+          ! deltamax for the selected particle.
+          do i=1,3 ! Three coordinates
+            call random_number(ran) ! Generate random variable
+            ! Store the new pos in rnew to later evaluate it
+            ! We are making the random be in (-1, 1) so it is in each dir.
+            rnew(i) = r(m,i) + deltamax * (2.d0 * ran - 1.d0)
+
+            ! Apply Periodic Boundary Conditions
+            if (rnew(i) .ge. L) rnew(i) = rnew(i) - L
+            if (rnew(i) .lt. 0.d0) rnew(i) = rnew(i) + L
+          end do
+          
+          ! Subroutine to check for overlapping
+          call overlapping(Np, m, r, rnew, L, nOverlap, radius)
+          if (nOverlap) then
+            r(i,1:3) = rnew(1:3)
+          else
+            Nmov = Nmov + 1
+            goto 998
+          end if
         end do
-        
-        call solap(Np, i, r, rnew, L, sol)
-        if (sol) then
-          Nmov = Nmov + 1
-          r(i,1:3) = rnew(1:3)
-        end if
       end do
-      print *, "Equilibration Acceptance: ", dble(Nmov)/Nmc
+      print *, "Acceptance rate: ",STEPS*Np/dble(Nmov+STEPS*Np)
 
-      ! 3. Production and g(r) Measurement
-      bines = 0.d0
+
+
+      ! ============================================
+      ! =   3. g(r) Production and Measurement     =
+      ! ============================================
+      bins = 0.d0
       Nmeas = 0
       Ntarg = 2000 ! Number of samples to take
       
       do k=1, Ntarg * 20 ! Sample every 20 MC steps
-        call random_number(ran)
+        CALL random_number(ran)
         i = int(ran * Np) + 1
         do j=1,3
-          call random_number(ran)
+          CALL random_number(ran)
           rnew(j) = r(i,j) + deltamax * (2.d0 * ran - 1.d0)
           if (rnew(j) .ge. L) rnew(j) = rnew(j) - L
           if (rnew(j) .lt. 0.d0) rnew(j) = rnew(j) + L
         end do
         
-        call solap(Np, i, r, rnew, L, sol)
-        if (sol) r(i,1:3) = rnew(1:3)
+        CALL OVERLAPPING(Np, i, r, rnew, L, nOverlap, radius)
+        if (nOverlap) r(i,1:3) = rnew(1:3)
 
         ! Take Measurement
         if (mod(k, 20) .eq. 0) then
           Nmeas = Nmeas + 1
           do n = 1, Np - 1
             do m = n + 1, Np
-              call distance(r(n,1:3), r(m,1:3), L, dist)
+              CALL DISTANCE(r(n,1:3), r(m,1:3), L, dist)
               if (dist .lt. L/2.d0) then
                 i = int(dist / rbin)
-                bines(i) = bines(i) + 2.d0
+                bins(i) = bins(i) + 2.d0
               end if
             end do
           end do
@@ -92,10 +119,10 @@
 
       ! 4. Normalization and Output
       rho = dble(Np) / (L**3)
-      Nbines = int((L/2.d0) / rbin)
+      Nbins = int((L/2.d0) / rbin)
       open(unit=10, file='gofr.dat', status='replace')
 
-      do i = 1, Nbines - 1
+      do i = 1, Nbins - 1
         r_in = dble(i) * rbin
         r_out = r_in + rbin
         r_centro = r_in + (rbin / 2.d0)
@@ -104,7 +131,7 @@
         V_capa = (4.d0/3.d0) * pi * (r_out**3 - r_in**3)
         
         ! g(r) = <n(r)> / (rho * V_shell)
-        g_r = bines(i) / (dble(Nmeas) * dble(Np) * rho * V_capa)
+        g_r = bins(i) / (dble(Nmeas) * dble(Np) * rho * V_capa)
         
         write(10, '(F10.4, F12.6)') r_centro, g_r
       end do
@@ -113,19 +140,23 @@
 
       end program
 
-      ! Subroutine: Check for Hard Sphere overlaps (radius = 1.0, sigma = 2.0)
-      subroutine solap(Np, i, r, rnew, L, sol)
-      implicit none
-      integer i, Np, k
-      real*8 r(Np, 3), rnew(3), L, dist
-      logical sol
 
-      sol = .true.
-      do k=1, Np
-        if (k .ne. i) then
-          call distance(r(k,1:3), rnew, L, dist)
-          if (dist .lt. 2.d0) then ! Overlap if distance < sigma
-            sol = .false.
+
+      ! Subroutine: Check for Hard Sphere overlaps
+      SUBROUTINE OVERLAPPING(Np, m, r, rnew, L, nOverlap, radius)
+      IMPLICIT NONE
+      INTEGER m, Np, i
+      REAL*8 r(Np, 3), rnew(3), L, dist, radius
+      LOGICAL nOverlap
+
+      ! We start by saying there is no overlap
+      nOverlap = .true.
+      do i=1, Np ! We check against every other particle
+        if (i .ne. m) then ! except ourselves
+          ! We calculate the distance between the new pos. and the part.
+          CALL DISTANCE(r(i,1:3), rnew, L, dist)
+          if (dist .lt. 2.d0*radius) then ! Overlap if distance < sigma
+            nOverlap = .false.
             return
           end if
         end if
@@ -133,10 +164,10 @@
       end subroutine
 
       ! Subroutine: Minimum Image Convention Distance
-      subroutine distance(r1, r2, L, dist)
-      implicit none
-      real*8 r1(3), r2(3), L, d(3), dist
-      integer k
+      SUBROUTINE DISTANCE(r1, r2, L, dist)
+      IMPLICIT NONE
+      REAL*8 r1(3), r2(3), L, d(3), dist
+      INTEGER k
 
       do k=1, 3
         d(k) = r1(k) - r2(k)
