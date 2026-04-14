a076e85ac11bf12eafc0444cfd1bb63cd6d4612f
Pablo <pablo.perfer@usal.es>
2026-04-14 12:56:55 +0200

    working theo

--- Iterative.f
+++ Iterative.f
@@ -1,19 +1,48 @@
       program MAIN
       implicit NONE
-      REAL*8 step, rmax, alpha, pi
-      INTEGER i,j,k,N
-      PARAMETER (rmax=8.d0, alpha=0.1d0)
-      PARAMETER (pi=4.d0*datan(1.d0), N=2**11, step=rmax/N)
-      real*8 fo(N), fn(N), c(N), h(N)
+      REAL step, rmax, alpha, pi, rho, error,limit,betae
+      INTEGER i,N
+      PARAMETER (rmax=4, alpha=0.1, betae=1, rho=0.490)
+      PARAMETER (pi=4*atan(real(1)), N=2**9, step=rmax/N)
+      PARAMETER (limit=0.01)
+      real fo(N), fn(N), c(N), h(N),g(N)
       print *, "the maximum valid k_r (in spherical coordinates) is "
      &, pi/step
       do i=1, N
-        fo(i)=exp(-100 * i*step)
+        fo(i)=0
       end do
-      call sinft(fo,N)
-      open(unit=11, file='fourier.dat', status='replace')
+      error=1
+      do while(error.gt.limit)
+        error=0
+        do i=1, N
+          h(i)=step*i*((fo(i)+1)*
+     &exp(-4*betae*((1/(i*step)**12)-(1/(i*step)**6)))-1)
+          c(i)=h(i)-step*i*fo(i)
+        end do
+        call sinft(h,N)
+        call sinft(c,N)
+        do i= 1, N
+          fn(i) = rho * h(i) * c(i) * 16 * pi / i * rmax
+        end do
+        call sinft(fn,N)
+        do i=1, N
+        fn(i) = fn(i)/ (2.0 * N * pi * i)
+        end do
+        fn= alpha * fn + (1-alpha) * fo
+        do i =1, N
+          error = error + abs(fn(i)-fo(i))
+        end do
+        do i=1, N
+          fo(i)=fn(i)
+        end do
+      end do
       do i=1, N
-        write(11, *) i*pi/rmax, fo(i)*step
+          g(i)=(fn(i)+1)*
+     &exp(-4*betae*((1/(i*step)**12)-(1/(i*step)**6)))
+      end do
+      open(unit=11, file='theogofr.dat', status='replace')
+      do i=1, N
+        write(11, *) i*step, g(i)
       end do
       close(11)
       end program
